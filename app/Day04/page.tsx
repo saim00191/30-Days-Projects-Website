51d7f1ae4551f7deab31b0d39787fd4b8f1210fc
saim00191 <saimamjad198@gmail.com>
2024-09-13 17:54:45 +0500

    update

---
+++ app/Day04/page.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-slate-600 min-h-screen flex justify-center items-center'>
+    <h1 className='text-4xl font-bold text-white'>DAY04 COMING SOON!!</h1>
+</div>
+  )
+}
