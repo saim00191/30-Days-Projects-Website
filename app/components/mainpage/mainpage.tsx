0872f70be2dee6dd33bffca4f35ae0cb4209dbdd
saim00191 <saimamjad198@gmail.com>
2024-09-13 16:57:23 +0500

    update

--- app/components/mainpage/mainpage.tsx
+++ app/components/mainpage/mainpage.tsx
@@ -17,7 +17,6 @@ export default function MainPage() {
           <div
             className="p-12 border-3 border-blue-800 border-[5px] m-2 flex items-center justify-center cursor-pointer hover:bg-blue-100"
             data-aos="fade-up" // Add AOS attribute for animation
-            data-aos-delay={`${index * 100}`} // Optional: Delay based on index
           >
             <h1 className="font-bold">{index < 9 ? `Day0${index + 1}` : `Day-${index + 1}`}</h1>
           </div>
