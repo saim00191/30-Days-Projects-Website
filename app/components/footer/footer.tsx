56c0f75de3d11a5889d0be055c1532bdf20425a9
saim00191 <saimamjad198@gmail.com>
2024-09-13 20:42:45 +0500

    update

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -4,31 +4,47 @@ import { FaLinkedinIn } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 export default function Footer() {
   return (
-      <div>
-          <footer className="text-gray-600 body-font">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      
-      <span className="ml-3 text-xl font-bold">30-Days-Of-Projects</span>
-    </a>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Developed By <span className='font-bold'>Saim Amjad</span>
-    </p>
-    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a href="https://www.facebook.com/saim.amjad.14" className="text-gray-500 text-2xl cursor-pointer hover:text-gray-900">
-        <FaFacebookF/>
-      </a>
-      <a href="https://github.com/saim00191" className="ml-4 text-gray-500 text-2xl cursor-pointer hover:text-gray-900">
-        <FaGithub/>
-      </a>
-      <a href="https://www.instagram.com/amjad_saim_9/" className="ml-4 text-gray-500 text-2xl cursor-pointer hover:text-gray-900">
-        <FaInstagram/>
-      </a>
-      <a href="https://www.linkedin.com/in/saim-raza-b7a7172b6/" className="ml-4 text-gray-500 text-2xl cursor-pointer hover:text-gray-900">
-        <FaLinkedinIn/>
-      </a>
-    </span>
-  </div>
-</footer>
+    <div>
+      <footer className="text-gray-600 body-font">
+        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <span className="ml-3 text-xl font-bold">30-Days-Of-Projects</span>
+          </a>
+          <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+            Developed By <span className="font-bold">Saim Amjad</span>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a
+              href="https://www.facebook.com/saim.amjad.14" 
+              className="text-gray-500 text-2xl cursor-pointer hover:text-gray-900"
+              target="_blank"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://github.com/saim00191" 
+              className="ml-4 text-gray-500 text-2xl cursor-pointer hover:text-gray-900"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.instagram.com/amjad_saim_9/" 
+              className="ml-4 text-gray-500 text-2xl cursor-pointer hover:text-gray-900"
+              target="_blank"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/saim-raza-b7a7172b6/" 
+              className="ml-4 text-gray-500 text-2xl cursor-pointer hover:text-gray-900"
+              target="_blank"
+            >
+              <FaLinkedinIn />
+            </a>
+          </span>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -7,21 +7,6 @@ export default function Navbar() {
     <Link href="/" className="flex items-center">
       <h1 className="font-bold text-xl sm:text-2xl">30 Days Projects</h1>
     </Link>
-    <nav>
-      <ul className="flex gap-4 font-semibold text-sm md:text-base">
-        <li className="hover:text-blue-600 transition-colors duration-300 cursor-pointer">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="hover:text-blue-600 transition-colors duration-300 cursor-pointer">
-          <Link href="/about">About</Link>
-        </li>
-        <li className="hover:text-blue-600 transition-colors duration-300 cursor-pointer">
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
   </header>
-  
-  
   )
 }
