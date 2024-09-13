3713e265d4992aa4c16d486e6aa10271c7fecd28
saim00191 <saimamjad198@gmail.com>
2024-09-13 18:21:27 +0500

    update

---
+++ app/Day27/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className='bg-slate-600 min-h-screen flex justify-center items-center'>
+      <h1 className='text-4xl font-bold text-white'>DAY27 COMING SOON!!</h1>
+  </div>
+    )
+  }
+  
