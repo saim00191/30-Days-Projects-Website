22c08a2024f7ed332a91ca693c4785610e0c8fea
saim00191 <saimamjad198@gmail.com>
2024-09-23 14:28:52 +0500

    update

--- app/Day08/components/color-picker-app.tsx
+++ app/Day08/components/color-picker-app.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 export default function ColorPicker() {
   const [color, setColor] = useState("#ffffff");
 
-  const handleChange = (event: any) => {
+  const handleChange = (event: {target :{value : string}}) => {
     setColor(event.target.value);
   };
 
